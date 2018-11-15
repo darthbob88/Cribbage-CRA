df44a8da0e8f11e825b2df224bdfba6091d87640
Raymond Robert Price <darthbob88@gmail.com>
2018-11-15 07:02:31 -0800

    Baby steps, including organizing everything.

---
+++ src/Cribbage/GameState.ts
@@ -0,0 +1,27 @@
+export interface GameState {
+    players: Player[];
+    // Torn between this, and making it a boolean field on the player.
+    dealer: Player;
+}
+export interface Hand {
+    playedCards: {
+        player: Player;
+        card: Card;
+    }[];
+}
+export interface Player {
+    id: string;
+    score: number;
+    cardsInHand: Card[];
+    cardsInCrib: Card[];
+}
+export interface Card {
+    suit: Suit;
+    number: number;
+}
+export enum Suit {
+    Hearts = "HEARTS",
+    Spades = "SPADES",
+    Diamonds = "DIAMONDS",
+    Clubs = "CLUBS"
+}
